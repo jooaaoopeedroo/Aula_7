688b8dc75ab0abeacc0392c37372811873ce0014
jooaaoopeedroo <joaop.jp75@gmail.com>
2025-12-09 22:45:12 +0000

    Execução

---
+++ ContadorFor.java
@@ -0,0 +1,8 @@
+public class ContadorFor {
+  public static void main(String[] args) {
+    for (int number = 0; number < 6; number ++) {
+        System.out.println("O código está sendo executado");
+        
+  }  
+}
+}
