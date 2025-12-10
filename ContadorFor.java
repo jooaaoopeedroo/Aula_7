788d0d345c7cf4252bd0f1ab9180b69382871f67
jooaaoopeedroo <joaop.jp75@gmail.com>
2025-12-10 00:34:03 +0000

    conta

--- ContadorFor.java
+++ ContadorFor.java
@@ -1,8 +1,23 @@
+import java.util.Scanner;
+
 public class ContadorFor {
-  public static void main(String[] args) {
-    for (int number = 0; number < 6; number ++) {
-        System.out.println("O código está sendo executado");
-        
-  }  
-}
+    public static void main(String[] args) {
+        Scanner sc = new Scanner (System.in);
+        System.out.println("--- Verificador de idade com Limitador! ---");
+        System.err.println();
+
+        System.err.println("Quantas vezes você quer verificsr:");
+        int quantidade = sc.nextInt();
+
+        System.out.println("Insira a idade da pessoa: ");
+        int idade = sc.nextInt();
+                
+                
+        if (idade >- 60){ 
+            System.out.println("Essa pessoa é Idosa!");
+        } else if (idade >= 18){
+            System.out.println("Essa pessoa é maior de idade!");
+        }
+      
+    }
 }
